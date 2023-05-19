ccc13093c90d3cfe3728da5cb28f37e9d98e647c
Jae hyeon <wjs2063@naver.com>
2023-05-19 22:29:26 +0900

    feat: add static file

--- server/routes/route.py
+++ server/routes/route.py
@@ -1,13 +1,16 @@
 from fastapi import *
+from fastapi.responses import *
 from typing import Annotated
 from ..model.database import *
-
-
+from fastapi.staticfiles import StaticFiles
 apps = APIRouter()
-
-
+apps.mount("/templates/",StaticFiles(directory = "/web/server/templates/static",html = True),name = "static")
 
 @apps.post("/api")
-async def post(db: Annotated[MongoClient ,Depends(get_db)]):
+async def test(db: Annotated[MongoClient ,Depends(get_db)]):
     db.user.insert_one({"example":1})
     return "hello World!"
+
+@apps.get("/")
+async def main():
+    return FileResponse("/web/server/templates/static/index.html")
