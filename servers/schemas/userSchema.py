eb54fa4005d00243af65cc8311f46c965ec3428d
Jae hyeon <wjs2063@naver.com>
2023-05-21 18:29:39 +0900

    fix: userSchema.py

--- servers/schemas/userSchema.py
+++ servers/schemas/userSchema.py
@@ -37,9 +37,7 @@ class UserData(BaseModel):
         orm_mode = True
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
-        json_encoders = {
-            "_id": str
-        }
+
 
 
 # function
